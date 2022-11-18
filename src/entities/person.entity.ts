97e96151f7ec2b5635610cfbbef321e531dccd58
blackkspydo <upadhyaya13kishor@gmail.com>
2022-11-18 22:38:42 +0545

    added email field

--- src/entities/person.entity.ts
+++ src/entities/person.entity.ts
@@ -1,16 +1,22 @@
-import { Entity, Column, PrimaryGeneratedColumn } from "typeorm"
+import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
 
 @Entity()
 export class Person {
-    @PrimaryGeneratedColumn('uuid')
-    id: string
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
 
-    @Column({
-        type: "varchar",
-    })
-    firstName: string
+  @Column({
+    type: 'varchar'
+  })
+  firstName: string;
 
-    @Column()
-    lastName: string
+  @Column()
+  lastName: string;
 
+  @Column({
+    type: 'varchar',
+    unique: true,
+    nullable: false
+  })
+  email: string;
 }
